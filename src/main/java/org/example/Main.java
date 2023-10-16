90cc2eab1cbdc95a6aa5bde0fa22c68d61fdc539
g21turbo <dg21stew@gmail.com>
2023-10-15 19:56:32 -0700

    More practice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,5 +25,27 @@ public class Main {
         String ageText = ageOfCar >= 10 ? "older" : "newer";
         System.out.println("The car is " + ageText);
 
+        System.out.println();
+
+        double var1 = 20.00;
+        double var2 = 80.00;
+
+        double var3 = var1 + var2 * 100;
+        System.out.println("Var3 = " + var3);
+
+        double var4 = var3%40.00;
+        System.out.println("Remainder = " + var4);
+
+        boolean zeroRemainder = var4 == 0 ? true : false;
+
+        System.out.println("zeroRemainder = " + zeroRemainder);
+
+        if (!zeroRemainder) {
+            System.out.println("Got some remainder");
+        }
+
+
+
+
     }
 }
